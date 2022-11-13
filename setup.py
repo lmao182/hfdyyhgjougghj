628cbd7a7982eca38c3b73b525b5dee269e7cde1
lmao182 <115547495+lmao182@users.noreply.github.com>
2022-11-13 16:47:50 +0700

    Vg

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ choose = input("Choose your environment: [0] pip / [1] pip3 : ")
 if choose == "0":
     os.system('pip install colorama')
     os.system('pip install pyinstaller')
+    os.system('pip install nuitka')
 elif choose == "1":
     os.system('pip3 install colorama')
     os.system('pip3 install pyinstaller')
+    os.system('pip3 install nuitka')
